c8979b0f241b64bfdebfea3fbb281dc6b66679a7
“Wiola” <Wioletta.cioc@gmail.com>
2019-11-23 14:35:28 +0100

    Tablica dwuwymiarowa pętla

--- src/pl/sda/podstawyjavy/PodstawyJavy.java
+++ src/pl/sda/podstawyjavy/PodstawyJavy.java
@@ -2,13 +2,22 @@ package pl.sda.podstawyjavy;
 
 public class PodstawyJavy {
     public static void main (String[]args){
+       char[][] mojaTablica = {
+               {'H','e','l','l','o'},
+               {' ', 'W','o','r','l','d'}
+       };
+
+       for (int i = 0; i < mojaTablica.length; i++){
+           System.out.print(mojaTablica[i]);
+       }
+
+    }
+    //wywołanie metody
+
+    void wydrukujArgumenty(String[] args) {
         for (int i = 0; i < args.length; i++) {
             System.out.println(args[i]);
 
         }
-
-
-
-        System.out.println("Hello World");
     }
 }
