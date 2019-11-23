4bce3bbb8231c3b71d8f3bceb8cdd381d5ad6e70
“Wiola” <Wioletta.cioc@gmail.com>
2019-11-23 13:47:01 +0100

    Pierwsza pętla w Javie

---
+++ src/pl/sda/podstawyjavy/PodstawyJavy.java
@@ -0,0 +1,14 @@
+package pl.sda.podstawyjavy;
+
+public class PodstawyJavy {
+    public static void main (String[]args){
+        for (int i = 0; i < args.length; i++) {
+            System.out.println(args[i]);
+
+        }
+
+
+
+        System.out.println("Hello World");
+    }
+}
