2fedc5d4456fc6387725ad952bc704e2e596460d
“Wiola” <Wioletta.cioc@gmail.com>
2019-11-24 10:14:25 +0100

    warunek trojargumentowy

--- src/pl/sda/podstawyjavy/PodstawyJavy.java
+++ src/pl/sda/podstawyjavy/PodstawyJavy.java
@@ -7,15 +7,7 @@ import java.sql.SQLOutput;
 public class PodstawyJavy {
     public static void main(String[] args) {
         int yearOfBirth =1980;
-        if(yearOfBirth < 1980){
-            System.out.println("Rok poniżej 1980");
-        }
-        else if (yearOfBirth > 1990){
-            System.out.println("Rok powyżej 1990");
-        }
-        else {
-            System.out.println("Rok pomiedzy 1980 a 1990");
-        }
+        String powitanie = (yearOfBirth == 1980) ? "true" : "false";
 
 
     }
