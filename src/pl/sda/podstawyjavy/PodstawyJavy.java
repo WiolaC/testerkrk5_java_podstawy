4aea1b4c423175d449496a20d020c5f1e27ada8e
“Wiola” <Wioletta.cioc@gmail.com>
2019-11-24 11:12:46 +0100

    scanner z licznikiem

--- src/pl/sda/podstawyjavy/PodstawyJavy.java
+++ src/pl/sda/podstawyjavy/PodstawyJavy.java
@@ -8,18 +8,22 @@ import java.util.Scanner;
 //ZCZYTYWANIE CZEGOS CO PODA UZYTKOWNIK NP ROK URODZENIA
 public class PodstawyJavy {
     public static void main(String[] args) {
-        for (int i= 0; i< 5; i++) {
-            Scanner scanner = new Scanner(System.in);
-            System.out.println("Proszę, podaj rok urodzenia:");
+        Scanner scanner= new Scanner(System.in);
 
-            int yearOfBirth = scanner.nextInt();
+        int licznikWykonan= 0;
+        int yearOfBirth = -1;
+        while (yearOfBirth <  2019 && licznikWykonan < 9) {
+            System.out.println((licznikWykonan + 1) + ".Proszę, podaj rok urodzenia:");
+
+            yearOfBirth = scanner.nextInt();
             String powitanie = (yearOfBirth == 1980) ?
-                    "Urodziłeś się w 1980"
-                    : "Nie urodziłeś się w 1980";
+                    "Urodziłeś się w 1980 - Brawo!"
+                    : "Urodziles sie w "+ yearOfBirth + " roku naszej ery";
             System.out.println(powitanie);
+            licznikWykonan++;
+
 
         }
-
     }
 }
 
