a3e9f2c859139b179d4ef1eb79f16a6045f3b0a1
“Wiola” <Wioletta.cioc@gmail.com>
2019-11-24 10:38:00 +0100

    scenner

--- src/pl/sda/podstawyjavy/PodstawyJavy.java
+++ src/pl/sda/podstawyjavy/PodstawyJavy.java
@@ -3,15 +3,22 @@ package pl.sda.podstawyjavy;
 import org.w3c.dom.ls.LSOutput;
 
 import java.sql.SQLOutput;
+import java.util.Scanner;
 
+//ZCZYTYWANIE CZEGOS CO PODA UZYTKOWNIK NP ROK URODZENIA
 public class PodstawyJavy {
     public static void main(String[] args) {
-        int yearOfBirth =1980;
-        String powitanie = (yearOfBirth == 1980) ?
-                "Urodziłeś się w 1980"
-                : "Nie urodziłeś się w 1980";
-        System.out.println(powitanie);
+        for (int i= 0; i< 5; i++) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Proszę, podaj rok urodzenia:");
 
+            int yearOfBirth = scanner.nextInt();
+            String powitanie = (yearOfBirth == 1980) ?
+                    "Urodziłeś się w 1980"
+                    : "Nie urodziłeś się w 1980";
+            System.out.println(powitanie);
+
+        }
 
     }
 }
