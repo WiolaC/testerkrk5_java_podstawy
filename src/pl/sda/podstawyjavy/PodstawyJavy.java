038c4dcc697bdffff457357905ab6af324c45dc0
“Wiola” <Wioletta.cioc@gmail.com>
2019-11-24 11:53:54 +0100

    pętla while zadanie 1

--- src/pl/sda/podstawyjavy/PodstawyJavy.java
+++ src/pl/sda/podstawyjavy/PodstawyJavy.java
@@ -5,27 +5,35 @@ import org.w3c.dom.ls.LSOutput;
 import java.sql.SQLOutput;
 import java.util.Scanner;
 
-//ZCZYTYWANIE CZEGOS CO PODA UZYTKOWNIK NP ROK URODZENIA
+
 public class PodstawyJavy {
     public static void main(String[] args) {
-        Scanner scanner= new Scanner(System.in);
+        boolean podanoPoprawnaWarosc= false;
+        Scanner scanner = new Scanner(System.in);
+        while (podanoPoprawnaWarosc == false) {
+            System.out.println("Podaj liczbę od 0-9");
+            int licznikWykonan = 0;
 
-        int licznikWykonan= 0;
-        int yearOfBirth = -1;
-        while (yearOfBirth <  2019 && licznikWykonan < 9) {
-            System.out.println((licznikWykonan + 1) + ".Proszę, podaj rok urodzenia:");
+            int liczba = scanner.nextInt();
 
-            yearOfBirth = scanner.nextInt();
-            String powitanie = (yearOfBirth == 1980) ?
-                    "Urodziłeś się w 1980 - Brawo!"
-                    : "Urodziles sie w "+ yearOfBirth + " roku naszej ery";
-            System.out.println(powitanie);
-            licznikWykonan++;
-
+            if (liczba >= 0 && liczba <= 9) {
+                podanoPoprawnaWarosc= true;
+                if (liczba == 3) {
+                    System.out.println("Dzień dobry");
+                } else if (liczba == 5) {
+                    System.out.println("Dobranoc");
+                } else {
+                    System.out.println("Standardowa wiadomość");
+                }
+            } else {
+                System.out.println("Podana wartość jest nieprawidłowa");
+            }
 
         }
     }
+
 }
+
 
 
 
